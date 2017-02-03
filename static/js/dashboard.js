a12d5f0539f0f9dd54f539ba036de385e20c6e00
glaaki <dreadpiratesean@gmail.com>
2017-02-02 18:27:09 -0600

    Moved index, progress on React.

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,4 +1,39 @@
+
+class HelloWorld extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: "grey"
+        }
+    }
+
+    toggleColor() {
+        if (this.state.color === "grey") {
+            this.setState({color: "black"})
+        } else {
+            this.setState({color: "grey"})
+        }
+    }
+
+    changeColor(event) {
+        this.setState({
+            color: event.target.value
+        });
+    }
+
+    render() {
+        const styleObj = {color: this.state.color}
+        return (
+            <div style={styleObj}>
+                <h2 onClick={this.toggleColor.bind(this)}>Hello, {this.props.name}.</h2>
+                <p>welcome to your dashboard</p>
+                <input value={this.state.color} onChange={this.changeColor.bind(this)}/>
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
-    <p>Hello world!</p>,
+    <HelloWorld name="Sean"/>,
     document.getElementById('app')
 );
