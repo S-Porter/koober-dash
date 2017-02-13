861dbe5764eeebace30021f54e8ef355b4f242da
glaaki <dreadpiratesean@gmail.com>
2017-02-12 20:00:41 -0600

    more react progress

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -12,45 +12,29 @@ class KooberDashboard extends React.Component {
 
 class WowApp extends React.Component {
     constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <HelloWorld name={this.props.name} />
-        );
-    }
-}
-
-class HelloWorld extends React.Component {
-    constructor(props) {
-        super(props);
+        super(props)
         this.state = {
-            color: "grey"
+            rep: {data: []}
         }
     }
 
-    toggleColor() {
-        if (this.state.color === "grey") {
-            this.setState({color: "black"})
-        } else {
-            this.setState({color: "grey"})
-        }
-    }
-
-    changeColor(event) {
-        this.setState({
-            color: event.target.value
-        });
-    }
+    componentDidMount() {
+        const self = this;
+        fetch("http://localhost:8080/api/wow/getrep/proudmoore/glaaki")
+            .then(function (response) {
+                return response.json();
+            }).then(function (json) {
+                self.setState({ rep: json })
+            });
+    };
 
     render() {
-        const styleObj = {color: this.state.color}
         return (
-            <div style={styleObj}>
-                <h2 onClick={this.toggleColor.bind(this)}>Hello, {this.props.name}.</h2>
-                <p>welcome to your dashboard</p>
-                <input value={this.state.color} onChange={this.changeColor.bind(this)}/>
-            </div>
+            <ul>
+                {this.state.rep.data.map(function (rep) {
+                    return <li>{rep.Name}: {rep.Value}</li>
+                })}
+            </ul>
         );
     }
 }
